c8a843b8a2bcc68e7b257ad4682eb8e89e7cb71b
Jacob Lang <jacoblang72@comcast.net>
2023-09-29 00:52:39 -0400

    finished put

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -43,4 +43,24 @@ router.post("/",async(req,res)=> {
     }
 })
 //!post
+//!put
+router.put("/:id",async(req,res)=> {
+    try {
+        const {id} = req.params;
+        const {title,contents} = req.body;
+        if (!title || !contents) {
+            res.status(400).json({ message: "Please provide title and contents for the post" })
+        } else {
+            const updatedPost = await PostData.update(id,{title : title, contents : contents})
+            if (!updatedPost) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            } else {
+                res.status(200).json({title : title, contents : contents, id : Number(id)})
+            }
+        }
+    } catch {
+        res.status(500).json({ message: "The post information could not be modified" })
+    }
+})
+//!put
 module.exports = router;
