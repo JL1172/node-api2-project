0bb1cf1da1f38a0e73db4ddbb534c5a202cfa08e
Jacob Lang <jacoblang72@comcast.net>
2023-09-29 00:26:54 -0400

    all wired up ready to go

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const express = require("express");
+const PostData = require("./posts-model");
+
+const router = express.Router();
+
+router.get("/",async(req,res)=> {
+    try {
+        const posts = await PostData.find();
+        res.status(200).json(posts)
+    } catch {
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+    }
+})
+
+module.exports = router;
